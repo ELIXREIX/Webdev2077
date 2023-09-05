45a9f7e98ab35e5c738cdf237722dcf5aa20c2e3
Lucilary <gofafafa1@gmail.com>
2023-09-05 16:16:11 +0700

    Save

--- Week1/script.js
+++ Week1/script.js
@@ -6,7 +6,7 @@ document.getElementById("submit-button").addEventListener("click", function (e) 
     e.preventDefault();
 
     // Replace with your email address
-    var emailAddress = "your-email@example.com";
+    var emailAddress = "gofafafa10@gmail.com";
 
     // Create a "mailto" link
     var mailtoLink = "mailto:" + emailAddress;
