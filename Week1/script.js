c1ffdd3180bcddf6f4ef0e17e3f5f83f0f1393c3
Lucilary <gofafafa1@gmail.com>
2023-09-05 16:21:34 +0700

    Save all

--- Week1/script.js
+++ Week1/script.js
@@ -6,10 +6,16 @@ document.getElementById("submit-button").addEventListener("click", function (e) 
     e.preventDefault();
 
     // Replace with your email address
-    var emailAddress = "gofafafa10@gmail.com";
+    var emailAddress = "your-email@example.com";
 
-    // Create a "mailto" link
-    var mailtoLink = "mailto:" + emailAddress;
+    // Get the message from the input field
+    var message = document.getElementById("email-message").value;
+
+    // Encode the message for inclusion in the "mailto" link
+    var encodedMessage = encodeURIComponent(message);
+
+    // Create a "mailto" link with the message
+    var mailtoLink = "mailto:" + emailAddress + "?body=" + encodedMessage;
 
     // Open the user's default email client
     window.location.href = mailtoLink;
