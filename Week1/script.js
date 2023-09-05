bc3f932d5649fa48ecede6893710ec62c27abe55
Lucilary <gofafafa1@gmail.com>
2023-09-05 16:11:01 +0700

    Save all

---
+++ Week1/script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Add an event listener to the Submit button
+document.getElementById("submit-button").addEventListener("click", function (e) {
+    // Prevent the default form submission
+    e.preventDefault();
+
+    // Replace with your email address
+    var emailAddress = "your-email@example.com";
+
+    // Create a "mailto" link
+    var mailtoLink = "mailto:" + emailAddress;
+
+    // Open the user's default email client
+    window.location.href = mailtoLink;
+});
